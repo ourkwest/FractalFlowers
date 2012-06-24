f73bcc19267a09d91ffe4f264c99c15f58f15170
Peter Westmacott <peter.westmacott@gmail.com>
2012-06-24 12:25:43 +0100

    Adding leaves to tree function. Aesthetic tweaking.

--- src/pw-play.clj
+++ src/pw-play.clj
@@ -36,7 +36,7 @@
     (drawplace p1)
     (addplace p2 p1)))
 	
-(def centre (struct place 400 400 0 1 (new Color 255 255 0 50)))
+(def centre (struct place 400 400 -0.4 0.9 (new Color 255 255 0 50)))
   
 (defn f-then
   [fn1 fn2]
@@ -55,27 +55,36 @@
         (recur (f arg) (dec n))))))
 		
 (defn f-tree
-  [fn1 fn2 n]
-  (loop [fna fn1 fnb fn2 n n]
-    (if (< n 1) 
-      fna
-      (recur (f-then fn1 (f-both fna fnb)) (f-then fn2 (f-both fnb fna)) (dec n)))))
+  ([fn1 fn2 n]
+    (loop [fna fn1 fnb fn2 n n]
+      (if (< n 1) 
+        fna
+        (recur (f-then fn1 (f-both fna fnb)) (f-then fn2 (f-both fnb fna)) (dec n)))))
+  ([fn1 fn2 leaf n]
+    (loop [l leaf n n]
+      (if (< n 1) 
+        l
+        (recur  (f-both (f-then fn1 l) (f-then fn2 l)) (dec n))))))
 		
 		
 (comment "fractal tree")
 (comment "                                     x y r s c")
-(def fn-a (partial draw-addplace (struct place 0 1 0.01 1 (new Color 255 127 0 5))))
-(def fn-b (partial draw-addplace (struct place 0 1 -0.01 1 (new Color 200 200 0 5))))
+(def fn-a (partial draw-addplace (struct place 0 1 0.005 1 (new Color 255 127 0 5))))
+(def fn-b (partial draw-addplace (struct place 0 1 -0.005 1 (new Color 200 200 0 5))))
 (def fn-s (partial draw-addplace (struct place 0 0 -0.1 0.75 (new Color 255 255 255 5))))
+
+(def fn-l1 (partial draw-addplace (struct place 0 10 -0.1 0.99 (new Color 0 255 0 50))))
+(def fn-l2 (partial draw-addplace (struct place 0 0 (/ Math/PI 2) 1 (Color/GREEN))))
+(def fn-leaf (f-then fn-l2 (f-dup fn-l1 200)))
 
 (def fn-c (f-then fn-s (f-dup fn-a 150)))
 (def fn-d (f-then fn-s (f-dup fn-b 175)))
 	  
-(def fn-f (f-tree fn-c fn-d 9))
+(def fn-f (f-then fn-c (f-tree fn-c fn-d fn-leaf 9)))
 
 (defn fun []
   (fn-f centre)
-  (fn-f (assoc centre :r (Math/PI)))
+  (fn-f (assoc centre :r (+ (Math/PI) (centre :r))))
   (done))
 
 (fun)
